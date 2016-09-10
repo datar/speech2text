af8838c14832af41c519322eabb7c997b54158c9
datar <tianxian@gmail.com>
2016-09-10 04:57:43 +0000

    fix bug in splitor.py

--- splitor.py
+++ splitor.py
@@ -14,7 +14,7 @@ def split_voice_file(voice_file, pos_file):
     else:
         return
     for p in positions:
-        filename = "%s_%04d_%s_%d_%d" % (basename, p[0], p[1], p[2], p[3])
+        filename = "%s_%04d_%s_%d_%d.%s" % (basename, p[0], p[1], p[2], p[3], fmt)
         voice[p[2]:p[3]].export(filename, format=fmt)
 
 
@@ -25,14 +25,14 @@ def get_pos_from_file(pos_file):
         for i, line in enumerate(infile.readlines()):
             people, end = line.split(' ')
             end = int(end)/100000*60000 + int(end)%100000
-            positions.append(i, people, start, end)
+            positions.append([i, people, start, end])
             start = end
     infile.close()
     return positions
 
 
 def main():
-    voice_file = "Benedict_Evans_Mono.wav"
+    voice_file = "../Benedict_Evans_Mono.wav"
     pos_file = "pos_test_file.txt"
     split_voice_file(voice_file, pos_file)
 
